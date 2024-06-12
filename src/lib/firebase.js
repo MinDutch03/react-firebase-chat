332aa024ae6ce4f75045f6f7b0ec2d2429dbbc44
Duc Nguyen <nguyenminhduc890@gmail.com>
2024-06-12 13:56:54 +0700

    update

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchat-d22fc.firebaseapp.com",
+  projectId: "reactchat-d22fc",
+  storageBucket: "reactchat-d22fc.appspot.com",
+  messagingSenderId: "888391115067",
+  appId: "1:888391115067:web:f858fe178ebacbd269d71e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
